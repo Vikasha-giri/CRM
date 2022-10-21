c8e77d87ec06ea49d603d94669aa343b9f46c307
Vikasha-giri <114727551+Vikasha-giri@users.noreply.github.com>
2022-10-21 17:31:40 +0530

    make it better

--- src/pages/Launchpad.js
+++ src/pages/Launchpad.js
@@ -105,7 +105,7 @@ const Launchpad = () => {
                             </ul>
                         </div>
                     </div>
-                    <ToastContainer />
+                    
                 </main>
             </div>
         </>
